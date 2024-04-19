43f03baee023ff2e4219f9eed22474a10212ad9c
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2024-04-19 14:04:43 +0300

    [RoadToReact] Extract components & props usage

--- 04.RoadToReact/hacker-stories/src/App.jsx
+++ 04.RoadToReact/hacker-stories/src/App.jsx
@@ -1,19 +1,79 @@
-import * as React from 'react'
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+import PropTypes from "prop-types";
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />      
+      <Search />
+
+      <hr />
+
+      <List list={stories} />
     </div>
-    );
+  );
 }
+
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+List.propTypes = {
+  list: PropTypes.array
+};
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+Item.propTypes = {
+  item: PropTypes.object
+};
 
 export default App;
