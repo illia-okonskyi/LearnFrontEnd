72610aab670368926e1c972a20b107093c76b04f
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2024-04-19 16:10:15 +0300

    [RoadToReact] Side-Effect hook

--- 04.RoadToReact/hacker-stories/src/App.jsx
+++ 04.RoadToReact/hacker-stories/src/App.jsx
@@ -22,7 +22,11 @@ function App() {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+    }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
